94dc652d72b705314adcc16263f1751ff69ef7f6
mharley12345 <info@mike-harley.tech>
2020-06-05 04:52:13 -0700

    creates db creates user_table

--- node_server/app.js
+++ node_server/app.js
@@ -20,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('messages',messageRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
