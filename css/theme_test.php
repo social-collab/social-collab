7fe6202a611f97335641988160b597665cb08e39
Donald Faulknor <64241217+social-collab@users.noreply.github.com>
2020-06-07 20:54:48 -0400

    Create theme_test.php

---
+++ css/theme_test.php
@@ -0,0 +1,15 @@
+<?php include('uikit.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+
+<!-- CREATE ALL ELEMENTS BETWEEN HERE -->
+
+<button class="uk-button uk-button-default"></button> <!-- This is a sample element -->
+
+<!-- CREATE ALL ELEMENTS BETWEEN HERE -->
+
+</body>
+</html>
